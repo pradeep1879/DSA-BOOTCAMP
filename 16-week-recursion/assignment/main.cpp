523f90e2c526732c1339d707641c88cab7af572f
Pradeep Kumar <pk6949137@gmail.com>
2026-05-15 23:32:26 +0530

    completed recursion assignment

--- 16-week-recursion/assignment/main.cpp
+++ 16-week-recursion/assignment/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 // Recursive function
@@ -164,6 +165,221 @@ void nthFibonacci(){
   cout << f9(n);
 }
 
+void f10(int arr[], int n){
+  if(n == 0){
+    return;
+  }
+  cout << arr[n-1] << " ";
+  f10(arr, n-1);
+}
+
+void printArrayReverse(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+
+  f10(arr, n);
+}
+
+void f11(int arr[], int n){
+  if(n == 0) return;
+  f11(arr, n-1);
+  cout << arr[n-1] << " ";
+}
+
+void printArray(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+
+  f11(arr, n);
+}
+
+int f12(int arr[], int n){
+  if(n == 1){
+    return arr[0];
+  }
+  int smallAns = f12(arr, n-1);
+  int ans = min(smallAns, arr[n - 1]);
+
+  return ans;
+}
+
+void minimumElement(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  cout << f12(arr, n);
+}
+
+int f13(int arr[], int n){
+  if(n == 1){
+    return arr[0];
+  }
+  int smallAns = f13(arr, n-1);
+  int ans = max(smallAns, arr[n - 1]);
+
+  return ans;
+}
+
+void maximumElement(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  cout << f13(arr, n);
+}
+
+long long f14(int arr[], int n){
+  if(n == 1){
+    return arr[0];
+  }
+
+  long long smallAns = f14(arr, n-1);
+  long long ans = smallAns + arr[n-1];
+
+  return ans;
+}
+
+void sumOfArray(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  
+  cout << f14(arr, n);
+}
+
+long long f15(int arr[], int n, long long x){
+  if(n == 0){
+    return -1;
+  }
+
+  // check first n-1 elements
+  int smallAns = f15(arr, n-1, x);
+
+  // if found earlier, return it
+  if(smallAns != -1){
+    return smallAns;
+  }
+  // check current element
+  if(arr[n-1] == x){
+    return n; // for 1-base indexing
+  }
+  return -1;
+}
+
+void firstOccurenceOfX(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  long long x;
+  cin >> x;
+
+  cout << f15(arr, n, x);
+  
+}
+
+
+long long f16(int arr[], int n, long long x){
+  if(n == 0){
+    return -1;
+  }
+  
+  if(arr[n-1] == x){
+    return n; // for 1-base indexing
+  }
+
+  int smallAns = f16(arr, n-1, x);
+  return smallAns;
+}
+
+void lastOccurenceOfX(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  long long x;
+  cin >> x;
+
+  cout << f16(arr, n, x);
+  
+}
+
+vector<int> ans;
+void f17(int arr[], int n, long long x){
+  if(n == 0){
+    return;
+  }
+  
+  f17(arr, n-1, x);
+
+  if(arr[n-1] == x){
+    ans.push_back(n);
+  }
+}
+
+void indicesOfX(){
+  int n;
+  cin >> n;
+  int arr[n];
+  for(int i = 0; i < n;  i++){
+    cin >> arr[i];
+  }
+  long long x;
+  cin >> x;
+
+  f17(arr, n, x);
+
+  // If not found
+    if(ans.size() == 0){
+      cout << -1;
+      return;
+    }
+
+  for(int i = 0; i < ans.size(); i++){
+    cout << ans[i] << " ";
+  }
+}
+
+void f18(int n, char src, char aux, char dest){
+  if(n == 0) return;
+
+  f18(n - 1, src, dest, aux);
+
+  cout << "Move " << src << " to " << dest << endl;
+
+  f18(n - 1, aux, src, dest);
+}
+
+void towerOfHanoi(){
+  int n;
+  cin >> n;
+  char A = 'A';
+  char B = 'B';
+  char C = 'C';
+
+  f18(n, A, B, C);
+}
+
 int main(){
   // printDecreasing();
   // printIncreasing ();
@@ -173,6 +389,15 @@ int main(){
   // sumOfFirstNnaturalNumbers();
   // power();
   // sumOfDigits();
-  nthFibonacci();
+  // nthFibonacci();
+  // printArrayReverse();
+  // printArray();
+  // minimumElement();
+  // maximumElement();
+  // sumOfArray();
+  // firstOccurenceOfX();
+  // lastOccurenceOfX();
+  // indicesOfX();
+  towerOfHanoi();
   return 0;
 }
