a3656503c9e744463b6b3a1ce0c787a6707df22a
Pradeep Kumar <pk6949137@gmail.com>
2026-05-29 22:11:22 +0530

    solve problems from sorting

--- 20-week-sorting/main.cpp
+++ 20-week-sorting/main.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main(){
-  
+int main() {
+  string s;
+  cin >> s;
+
+  vector<int> freq(26, 0);
+
+  for (char c : s) {
+      freq[c - 'a']++;
+  }
+
+  string ans;
+
+  for (int i = 0; i < 26; i++) {
+      ans.append(freq[i], char('a' + i));
+  }
+
+  cout << ans << '\n';
+
+  return 0;
 }
