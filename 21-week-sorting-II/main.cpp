f85fe7c409ab8281b3b23484e5c4925bf8a9af4c
Pradeep Kumar <pk6949137@gmail.com>
2026-06-01 23:21:47 +0530

    solve assignment problems

--- 21-week-sorting-II/main.cpp
+++ 21-week-sorting-II/main.cpp
@@ -1,6 +1,30 @@
-#include <iostream>
+#include <iostream>;
 using namespace std;
 
-int main(){
-  
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int N;
+    cin >> N;
+
+    vector<int> freq(101, 0);
+
+    for (int i = 0; i < N; i++) {
+        int age;
+        cin >> age;
+        freq[age]++;
+    }
+
+    bool first = true;
+    for (int age = 0; age <= 100; age++) {
+        while (freq[age]--) {
+            if (!first) cout << ' ';
+            cout << age;
+            first = false;
+        }
+    }
+
+    cout << '\n';
+    return 0;
 }
