07d1ac265fd1b99d1d5ca40125f8cdd1dc1294d9
Pradeep Kumar <pk6949137@gmail.com>
2026-07-16 11:55:24 +0530

    solve assignment

--- week-23.1-LinkedList-I/assignment/main.cpp
+++ week-23.1-LinkedList-I/assignment/main.cpp
@@ -1,6 +1,114 @@
 #include <iostream>
 using namespace std;
 
-int main(){
+struct Node {
+    int val;
+    Node* next;
+    Node(int v) : val(v), next(nullptr) {}
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() : head(nullptr) {}
+
+    void build(int n) {
+        head = nullptr;
+        Node* tail = nullptr;
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            Node* node = new Node(x);
+            if (!head) {
+                head = tail = node;
+            } else {
+                tail->next = node;
+                tail = node;
+            }
+        }
+    }
+
+    ~LinkedList() {
+        Node* cur = head;
+        while (cur) {
+            Node* nxt = cur->next;
+            delete cur;
+            cur = nxt;
+        }
+    }
+};
+
+/*
+    Implement the function below.
+    It should print the linked list in the required format.
+*/
+void printList(Node* head){
   
+  while(head != nullptr){
+    cout << head->val << " -> ";
+    head = head->next;
+  }
+  cout << "X";
+};
+
+int findLength(Node* head){
+  int count = 0;
+  while(head != nullptr){
+    count++;
+    head = head->next;
+  }
+  return count;
+}
+
+int countX(Node* head, int x){
+  int count = 0;
+  while(head != nullptr){
+    if(head->val == x){
+      count++;
+    }
+    head = head->next;
+  }
+  return count;
+}
+
+int findMin(Node* head){
+  if(head == nullptr){
+    return -1;
+  }
+  int ans = head->val;
+  while(head != nullptr){
+    ans = min(ans, head->val);
+    head = head->next;
+  }
+  return ans;
+}
+
+int findKth(Node* head, int k){
+  
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    LinkedList ll;
+    ll.build(n);
+
+    // printList(ll.head);
+    // cout << findLength(ll.head);
+
+    // int x;
+    // cin >> x;
+    // cout << countX(ll.head, x) << "\n";
+
+    // cout << findMin(ll.head) << "\n";
+
+    int k;
+    cin >> k;
+    cout << findKth(ll.head, k) << "\n";
+    return 0;
 }
