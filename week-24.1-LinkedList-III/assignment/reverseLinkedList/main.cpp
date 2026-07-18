5522bcc27975e827bce1fd55c7a126d80edb77f3
Pradeep Kumar <pk6949137@gmail.com>
2026-07-18 23:01:18 +0530

    solve reverse the linked list problem

---
+++ week-24.1-LinkedList-III/assignment/reverseLinkedList/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+
+    Node(int v)
+    {
+        val = v;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+
+    Node* head;
+
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void build(int n)
+    {
+        Node* tail = nullptr;
+
+        for(int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+
+            Node* node = new Node(x);
+
+            if(!head)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail->next = node;
+                tail = node;
+            }
+        }
+    }
+};
+
+/*
+    Implement only the function below.
+    Reverse the singly linked list and return the head of the reversed list.
+*/
+
+Node* reverseList(Node* head){
+  Node* prev = NULL;
+  auto curr = head;
+  
+
+  while(curr != NULL){
+    auto next = curr->next;
+    curr->next = prev;
+    prev = curr;
+    curr = next;
+  }
+    return prev;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    LinkedList ll;
+
+    ll.build(n);
+
+    ll.head = reverseList(ll.head);
+
+    Node* cur = ll.head;
+
+    bool first = true;
+
+    while(cur)
+    {
+        if(!first)
+        {
+            cout << ' ';
+        }
+
+        first = false;
+
+        cout << cur->val;
+
+        cur = cur->next;
+    }
+
+    cout << '\n';
+
+    return 0;
+}
