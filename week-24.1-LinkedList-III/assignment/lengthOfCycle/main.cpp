84057bd36a25f9864f5a0c3ac01501dadb005590
Pradeep Kumar <pk6949137@gmail.com>
2026-07-19 23:27:50 +0530

    solve problem length of cycle

---
+++ week-24.1-LinkedList-III/assignment/lengthOfCycle/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+
+    Node(int v)
+    {
+        val = v;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+
+    Node* head;
+    Node* tail;
+    vector<Node*> nodes;
+
+    LinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void build(int n)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+
+            Node* node = new Node(x);
+
+            nodes.push_back(node);
+
+            if(!head)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail->next = node;
+                tail = node;
+            }
+        }
+    }
+};
+
+/*
+    Implement only the function below.
+    Return the number of nodes in the loop, or 0 if there is no loop.
+*/
+
+int lengthOfLoop(Node* head){
+  Node* slow = head;
+  Node* fast = head;
+
+  //Detect cycle
+  while(fast != NULL and fast->next != NULL){
+    slow = slow->next;
+    fast = fast->next->next;
+
+    if(slow == fast){
+      // Count cycle length
+      int count = 0;
+      fast = fast->next;
+
+      while(slow != fast){
+        count++;
+        fast = fast->next;
+      }
+      return count;
+    }
+  }
+  return 0;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    LinkedList ll;
+
+    ll.build(n);
+
+    int pos;
+    cin >> pos;
+
+    if(pos != -1)
+        ll.tail->next = ll.nodes[pos];
+
+    cout << lengthOfLoop(ll.head) << '\n';
+
+    return 0;
+}
