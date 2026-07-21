acd2fcbcc83c08ee7e86c5630b070bfada88143e
Pradeep Kumar <pk6949137@gmail.com>
2026-07-21 22:56:54 +0530

    solve palindrome problem

---
+++ week-24.1-LinkedList-III/assignment/PalindromeLinkedList/main.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int val;
+    Node* next;
+
+    Node(int v)
+    {
+        val = v;
+        next = nullptr;
+    }
+};
+
+class LinkedList {
+public:
+
+    Node* head;
+
+    LinkedList()
+    {
+        head = nullptr;
+    }
+
+    void build(int n)
+    {
+        Node* tail = nullptr;
+
+        for(int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+
+            Node* node = new Node(x);
+
+            if(!head)
+            {
+                head = tail = node;
+            }
+            else
+            {
+                tail->next = node;
+                tail = node;
+            }
+        }
+    }
+};
+
+/*
+    Implement only the function below.
+    Return true if the linked list is a palindrome, otherwise false.
+*/
+
+bool isPalindrome(Node* head){
+  
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    LinkedList ll;
+
+    ll.build(n);
+
+    cout << (isPalindrome(ll.head) ? "true" : "false") << '\n';
+
+    return 0;
+}
