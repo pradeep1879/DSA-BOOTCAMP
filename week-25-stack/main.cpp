16c089d03c0919edc7836a330bca4fb40f67d82a
Pradeep Kumar <pk6949137@gmail.com>
2026-07-25 00:23:35 +0530

    implementing stack with LinkedList

--- week-25-stack/main.cpp
+++ week-25-stack/main.cpp
@@ -1,37 +1,160 @@
 #include <iostream>
 using namespace std;
 
-const int MAX_SIZE = 10^6;
-const int INVALID_OPERATION = 10^6;
+// ------------------ Stack using Array ------------------
 
-class Stack{
-  int arr[MAX_SIZE];
-  int ptr = -1;
+const int MAX_SIZE = 1000000;
+const int INVALID_OPERATION = -1;
 
-  void push(int x){
-    if(size() == MAX_SIZE) return;
-    ptr++;
-    arr[ptr] = x;
-  }
+class Stack {
+    int arr[MAX_SIZE];
+    int ptr;
 
-  void pop(){
-    if(!empty());
-    ptr--;
-  }
+public:
+    Stack() {
+        ptr = -1;
+    }
 
-  int peek(){
-    return empty() ? INVALID_OPERATION : arr[ptr];
-  }
+    void push(int x) {
+        if (size() == MAX_SIZE)
+            return;
 
-  bool empty(){
-    return ptr == -1;
-  }
+        arr[++ptr] = x;
+    }
 
-  int size(){
-    return ptr + 1;
-  }
+    void pop() {
+        if (empty())
+            return;
+
+        ptr--;
+    }
+
+    int peek() {
+        if (empty())
+            return INVALID_OPERATION;
+
+        return arr[ptr];
+    }
+
+    bool empty() {
+        return ptr == -1;
+    }
+
+    int size() {
+        return ptr + 1;
+    }
 };
 
-int main(){
+// ------------------ Linked List ------------------
 
+class Node {
+public:
+    int val;
+    Node* next;
+
+    Node(int val) {
+        this->val = val;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList {
+    Node* head;
+    int sz;
+
+public:
+    LinkedList() {
+        head = nullptr;
+        sz = 0;
+    }
+
+    void addAtHead(int x) {
+        Node* newNode = new Node(x);
+        newNode->next = head;
+        head = newNode;
+        sz++;
+    }
+
+    int deleteAtHead() {
+        if (head == nullptr)
+            return INVALID_OPERATION;
+
+        Node* temp = head;
+        int value = temp->val;
+
+        head = head->next;
+        delete temp;
+        sz--;
+
+        return value;
+    }
+
+    int getHead() {
+        if (head == nullptr)
+            return INVALID_OPERATION;
+
+        return head->val;
+    }
+
+    bool empty() {
+        return head == nullptr;
+    }
+
+    int size() {
+        return sz;
+    }
+};
+
+// ------------------ Stack using Linked List ------------------
+
+class MyStack {
+    LinkedList list;
+
+public:
+    void push(int x) {
+        list.addAtHead(x);
+    }
+
+    int pop() {
+        return list.deleteAtHead();
+    }
+
+    int top() {
+        return list.getHead();
+    }
+
+    bool empty() {
+        return list.empty();
+    }
+
+    int size() {
+        return list.size();
+    }
+};
+
+// ------------------ Driver ------------------
+
+int main() {
+
+    MyStack st;
+
+    st.push(10);
+    st.push(20);
+    st.push(30);
+
+    cout << "Top: " << st.top() << endl;
+    cout << "Size: " << st.size() << endl;
+
+    cout << "Pop: " << st.pop() << endl;
+    cout << "Top: " << st.top() << endl;
+
+    st.push(40);
+
+    while (!st.empty()) {
+        cout << st.pop() << " ";
+    }
+
+    cout << endl;
+
+    return 0;
 }
