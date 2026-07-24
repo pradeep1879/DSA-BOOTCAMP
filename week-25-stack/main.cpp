a2b4f7739257cff475df246837d3d95b06e7a3d7
Pradeep Kumar <pk6949137@gmail.com>
2026-07-24 23:58:00 +0530

    implementing stack

--- week-25-stack/main.cpp
+++ week-25-stack/main.cpp
@@ -1,7 +1,33 @@
 #include <iostream>
 using namespace std;
 
+const int MAX_SIZE = 10^6;
+
+class Stack{
+  int arr[MAX_SIZE];
+  int ptr = -1;
+  void push(int x){
+    ptr++;
+    arr[ptr] = x;
+  }
+
+  void pop(){
+    ptr--;
+  }
+
+  int peek(){
+    return arr[ptr];
+  }
+
+  bool empty(){
+    return ptr == -1;
+  }
+
+  int size(){
+    return ptr + 1;
+  }
+};
 
 int main(){
-  
+
 }
