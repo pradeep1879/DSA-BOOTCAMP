88d97cd7268056fdaf073016f2939c161ca9ebdd
Pradeep Kumar <pk6949137@gmail.com>
2026-07-25 00:04:18 +0530

    implementing stack with edgecases

--- week-25-stack/main.cpp
+++ week-25-stack/main.cpp
@@ -2,21 +2,25 @@
 using namespace std;
 
 const int MAX_SIZE = 10^6;
+const int INVALID_OPERATION = 10^6;
 
 class Stack{
   int arr[MAX_SIZE];
   int ptr = -1;
+
   void push(int x){
+    if(size() == MAX_SIZE) return;
     ptr++;
     arr[ptr] = x;
   }
 
   void pop(){
+    if(!empty());
     ptr--;
   }
 
   int peek(){
-    return arr[ptr];
+    return empty() ? INVALID_OPERATION : arr[ptr];
   }
 
   bool empty(){
